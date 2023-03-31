1d2b22726afcb8c1a83027a79f7737d0a73ad720
Armando DBG <bollain@gmail.com>
2023-03-30 17:18:41 -0700

    Update to 10.0

---
+++ samples/viewing/viewing-with-webviewer-office-editor/viewing-with-webviewer-office-editor.js
@@ -0,0 +1,8 @@
+WebViewer(
+  {
+    path: '../../../lib',
+    enableOfficeEditing: true,
+    initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/legal-contract.docx',
+  },
+  document.getElementById('viewer')
+);
